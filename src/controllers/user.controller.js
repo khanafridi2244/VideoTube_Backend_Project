1ac4d0c2713f08377e81a136f0d603510054088e
sikandar <sikanderafridi.854@gmail.com>
2026-06-27 02:29:57 +0500

    logic building ...

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,17 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
-
+import {ApiError} from "../utils/ApiError.js"
+import {User} from "../models/user.models.js"
 
 const registerUser = asyncHandler( async (req, res) => {
-    res.status(200).json({
-        message: "ok"
-    })
+   
+
+
+
+
+    const {fullName, email, username, password} = req.body
+    console.log("email: ", email)
+
+     
 })
 
 
